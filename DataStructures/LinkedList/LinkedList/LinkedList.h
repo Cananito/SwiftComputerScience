f088e074f8aefdfca2c9ad7d5ed68147b04dfa97
Rogelio Gudino <cananito@gmail.com>
2015-02-19 21:32:29 -0800

    Added stuff back.

---
+++ DataStructures/LinkedList/LinkedList/LinkedList.h
@@ -0,0 +1,19 @@
+//
+//  LinkedList.h
+//  LinkedList
+//
+//  Created by Rogelio Gudino on 2/19/15.
+//  Copyright (c) 2015 Rogelio Gudino. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LinkedList.
+FOUNDATION_EXPORT double LinkedListVersionNumber;
+
+//! Project version string for LinkedList.
+FOUNDATION_EXPORT const unsigned char LinkedListVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinkedList/PublicHeader.h>
+
+
