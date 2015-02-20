f1be5d19324c6670bae636f36e85a20afa45caca
Rogelio Gudino <cananito@gmail.com>
2015-02-19 21:34:50 -0800

    Added a Queue framework project.

---
+++ DataStructures/Queue/Queue/Queue.h
@@ -0,0 +1,19 @@
+//
+//  Queue.h
+//  Queue
+//
+//  Created by Rogelio Gudino on 2/19/15.
+//  Copyright (c) 2015 Rogelio Gudino. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Queue.
+FOUNDATION_EXPORT double QueueVersionNumber;
+
+//! Project version string for Queue.
+FOUNDATION_EXPORT const unsigned char QueueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Queue/PublicHeader.h>
+
+
---
+++ DataStructures/Queue/QueueTests/QueueTests.swift
@@ -0,0 +1,19 @@
+//
+//  QueueTests.swift
+//  QueueTests
+//
+//  Created by Rogelio Gudino on 2/19/15.
+//  Copyright (c) 2015 Rogelio Gudino. All rights reserved.
+//
+
+import XCTest
+
+class QueueTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
