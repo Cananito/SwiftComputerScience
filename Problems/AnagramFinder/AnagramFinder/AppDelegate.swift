a73a19b14361abc4905a4158498e9539a322ed6c
Rogelio Gudino <cananito@gmail.com>
2015-05-10 08:35:14 -0700

    Tiny performance improvements. Need to cosmetically improve the anagrams list.

--- Problems/AnagramFinder/AnagramFinder/AppDelegate.swift
+++ Problems/AnagramFinder/AnagramFinder/AppDelegate.swift
@@ -11,8 +11,10 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTableViewDelegate {
     @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var progressIndicator: NSProgressIndicator!
     @IBOutlet weak var textScrollView: NSScrollView!
     @IBOutlet weak var findAnagramsButton: NSButton!
+    @IBOutlet weak var anagramsTableViewScrollContainer: NSScrollView!
     @IBOutlet weak var anagramsTableView: NSTableView!
     var textView: NSTextView! {
         get {
@@ -24,9 +26,17 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTab
     // MARK: Action Methods
     @IBAction func findAnagrams(sender: AnyObject!) {
         if let string = self.textView.string as String? {
-            let words = split(string, isSeparator: { $0 == " " })
-            self.currentClusterArray = clusterArrayOfWords(words)
-            self.anagramsTableView.reloadData()
+            self.showLoadingUI()
+            
+            NSOperationQueue().addOperationWithBlock { () -> Void in
+                let words = split(string, isSeparator: { $0 == "\n" })
+                self.currentClusterArray = clusterArrayOfWords(words)
+                
+                NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                    self.hideLoadingUI()
+                    self.anagramsTableView.reloadData()
+                })
+            }
         }
     }
     
@@ -37,7 +47,9 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTab
     
     // MARK: NSTableViewDelegate Methods
     func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        let key = self.currentClusterArray.keys.array[row]
+        let index = advance(self.currentClusterArray.startIndex, row)
+        let key = self.currentClusterArray.keys[index]
+        
         var numberOfOccurrences = 0
         if let words = self.currentClusterArray[key] {
             numberOfOccurrences = count(words)
@@ -45,10 +57,28 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTab
         
         let tableCellView = tableView.makeViewWithIdentifier("AnagramCell", owner: self) as? NSTableCellView
         tableCellView?.textField?.stringValue = "\(key): \(numberOfOccurrences)"
+        
         return tableCellView
     }
     
     func tableView(tableView: NSTableView, selectionIndexesForProposedSelection proposedSelectionIndexes: NSIndexSet) -> NSIndexSet {
         return NSIndexSet()
+    }
+    
+    // MARK: Private Methods
+    private func showLoadingUI() {
+        self.textScrollView.hidden = true
+        self.anagramsTableView.hidden = true
+        self.anagramsTableViewScrollContainer.hidden = true
+        self.findAnagramsButton.enabled = false
+        self.progressIndicator.startAnimation(self)
+    }
+    
+    private func hideLoadingUI() {
+        self.textScrollView.hidden = false
+        self.anagramsTableView.hidden = false
+        self.anagramsTableViewScrollContainer.hidden = false
+        self.findAnagramsButton.enabled = true
+        self.progressIndicator.stopAnimation(self)
     }
 }
