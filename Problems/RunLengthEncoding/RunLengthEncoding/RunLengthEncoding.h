245622c82e1cb5d943b992bdda45746f4268a80c
Rogelio Gudino <cananito@gmail.com>
2015-11-02 19:17:54 -0800

    Made a Run-Lenght Encoding framework. Only implemented the generator.

---
+++ Problems/RunLengthEncoding/RunLengthEncoding/RunLengthEncoding.h
@@ -0,0 +1,19 @@
+//
+//  RunLengthEncoding.h
+//  RunLengthEncoding
+//
+//  Created by Rogelio Gudino on 11/2/15.
+//  Copyright © 2015 Rogelio Gudino. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RunLengthEncoding.
+FOUNDATION_EXPORT double RunLengthEncodingVersionNumber;
+
+//! Project version string for RunLengthEncoding.
+FOUNDATION_EXPORT const unsigned char RunLengthEncodingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RunLengthEncoding/PublicHeader.h>
+
+
